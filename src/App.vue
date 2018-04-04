59eb98282b382fd822c92d0735f28fb32cfc6668
wangks <wangkangsen168@163.com>
2018-04-04 19:35:36 +0800

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  created() {
+    // 调用API从本地缓存中获取数据
+    const logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
+
+    console.log('app created and cache logs by setStorageSync');
+  },
+};
+</script>
+
+<style lang="less">
+@import (reference) './assets/less/index.less';
+/*
+Blue #20A0FF Success #13CE66 Warning #F7BA2A Danger #FF4949
+Black #1F2D3D  Light Black #324057 Extra Light Black #475669
+Dark White #F9FAFC Extra Light Gray #d1dbe5
+*/ 
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  background-color: @color-body-bg;
+  color: @color-font1;
+  overflow: hidden;
+}
+/* this rule will be remove */
+* {
+  transition: width 2s;
+  -moz-transition: width 2s;
+  -webkit-transition: width 2s;
+  -o-transition: width 2s;
+}
+
+page {
+  height: 100%;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,232 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+    <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <div><i class="icon icon-star1"></i></div>
+      <div>66</div>
+    </div>
+    <div class="progress-wrap">
+      <div class="progress-circle">
+        <div class="pie_left"><div class="left" :style="{transform: leftRotate}"></div></div>
+        <div class="pie_right"><div class="right" :style="{transform: rightRotate}"></div></div>
+        <div class="mask">
+          <p>第{{checkPoint * 1}}关</p>
+        </div>
+      </div>
+      <p class="progress-totle">共10关</p>
+      <div class="progress-button">开始闯关</div>
+    </div>
+    <ul class="column">
+      <li>
+        <p><i class="icon icon-LC_icon_photo_fill"></i></p>
+        <p>错题库</p>
+      </li>
+      <li>
+        <p><i class="icon icon-LC_icon_photo_fill"></i></p>
+        <p>PK场</p>
+      </li>
+      <li>
+        <p><i class="icon icon-LC_icon_photo_fill"></i></p>
+        <p>终极考核室</p>
+      </li>
+    </ul>
+    <div class="rank">
+      <p><i class="icon icon-LC_icon_photo_fill"></i></p>
+      <p>排行榜</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card';
+
+export default {
+  data() {
+    return {
+      motto: 'Hello World',
+      userInfo: {},
+      checkPoint: 1,
+    };
+  },
+
+  components: {
+    card,
+  },
+  computed: {
+    leftRotate() {
+      return this.deg > 180 ? `rotate(${this.deg - 180}deg)` : 0;
+    },
+    rightRotate() {
+      return this.deg > 180 ? 'rotate(180deg)' : `rotate(${this.deg}deg)`;
+    },
+    deg() {
+      return (this.checkPoint / 10) * 360;
+    },
+  },
+  methods: {
+    bindViewTap() {
+      const url = '../logs/main';
+      wx.navigateTo({ url });
+    },
+    getUserInfo() {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo;
+            },
+          });
+        },
+      });
+    },
+    clickHandle(msg, ev) {
+      console.log('clickHandle:', msg, ev);
+    },
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import (reference) '../../assets/less/index.less';
+@import '../../assets/iconfont/iconfont.wxss';
+.userinfo {
+  display: flex;
+  width: 100%;
+  flex: 1 0 auto;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .userinfo-avatar {
+    width: 64px;
+    height: 64px;
+    margin: 10px;
+    border-radius: 50%;
+  }
+
+  div {
+    font-size: 16px;
+    width: 64px;
+    margin-left: 10px;
+    text-align: center;
+  }
+
+  i {
+    font-size: 30px;
+  }
+}
+
+.progress-wrap {
+  flex: 1 0 240px;
+  position: relative;
+  width: 100%;
+    .progress-circle {
+    width: 200px; 
+    height: 200px; 
+    position: absolute; 
+    border-radius: 50%; 
+    background: @color-progress;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+
+    .pie_left, .pie_right {
+      width: 200px; 
+      height: 200px; 
+      position: absolute; 
+      top: 0;
+      left: 0;
+    }
+    .left, .right {
+      display: block; 
+      width:200px; 
+      height:200px; 
+      background:@color-shallow-gray; 
+      border-radius: 50%; 
+      position: absolute; 
+      top: 0; 
+      left: 0; 
+      transform: rotate(0deg); 
+    }
+    .pie_right, .right { 
+      clip:rect(0,auto,auto,100px); 
+    }
+    .pie_left, .left { 
+      clip:rect(0,100px,auto,0); 
+    }
+    .mask { 
+      width: 160px; 
+      height: 160px; 
+      border-radius: 50%; 
+      left: 20px; 
+      top: 20px; 
+      background: @color-white;
+      position: absolute; 
+      text-align: center; 
+      line-height: 160px; 
+      font-size: 16px;
+    }
+  }
+
+  .progress-totle {
+    position: absolute;
+    bottom: 36%;
+    left: 42%;
+    color: @color-font-disabled;
+  }
+
+  .progress-button {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+    font-size: 20px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+}
+
+.column {
+  flex: 1 0 auto;
+  width: 100%;
+  display: flex;
+  padding-top: 30px;
+  li {
+    flex: 1 0 33.3%;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    font-size: 16px;
+    img {
+      width: 100px;
+      height: 100px;
+    }
+
+    i {
+      font-size: 60px;
+    }
+  } 
+}
+
+.rank {
+  flex: 1 0 auto;
+  width: 100%;
+  flex-direction: column;
+  text-align: center;
+  font-size: 16px;
+  padding-bottom: 30px;
+  img {
+    width: 50px;
+    height: 50px;
+  }
+  i {
+    font-size: 40px;
+  }
+}
+
+</style>
